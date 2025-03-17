ac10e3c9451e9140b4b88c2ae27e3f0e0bb2a353
Nataliya Trondina <trondina.nataliya@yandex.ru>
2025-03-17 12:33:03 +0400

    исправлено объявление переменных

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let num = 266219;
+const num = 266219;
 
-let arr = num.toString().split("");
-let multiply = arr.reduce((acc, item) => acc * item, 1);
+const arr = num.toString().split("");
+const multiply = arr.reduce((acc, item) => acc * item, 1);
 console.log(multiply);
 
-let newNum = multiply ** 3;
-let newArr = newNum.toString().split("").slice(0, 2);
+const newNum = multiply ** 3;
+const newArr = newNum.toString().split("").slice(0, 2);
 
 console.log(newArr.toString());
