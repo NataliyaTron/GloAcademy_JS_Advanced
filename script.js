a600ddd9d0de79a0ff3606233e21a60732857a0f
Nataliya Trondina <trondina.nataliya@yandex.ru>
2025-03-27 07:17:45 +0400

    выполнено задание шестого урока

--- script.js
+++ script.js
@@ -1,21 +1,51 @@
 "use strict";
 
-const checkString = (str) => {
-  if (typeof str !== "string") {
-    console.log("Ошибка! В качестве аргумента передана не строка");
-    return;
-  } else {
-    str = str.trim();
+const guessNumber = () => {
+  const botNumber = Math.floor(Math.random() * 100) + 1;
+  let attempts = 10;
 
-    if (str.length > 30) {
-      str = str.slice(0, 30) + "...";
+  const checkNumber = () => {
+    if (attempts === 0) {
+      if (confirm("Попытки закончились, хотите сыграть еще?")) {
+        guessNumber();
+      } else {
+        alert("Спасибо за игру!");
+      }
+      return;
     }
-    return str;
-  }
+
+    let userInput = prompt(
+      "Угадай число от 1 до 100. Осталось попыток - " + attempts
+    );
+
+    if (userInput === null) {
+      alert("Игра окончена");
+      return;
+    }
+
+    const userNumber = +userInput;
+
+    if (isNaN(userNumber)) {
+      alert("Введи число!");
+      checkNumber();
+      return;
+    } else if (userNumber > botNumber) {
+      attempts--;
+      alert("Загаданное число меньше, осталось попыток " + attempts);
+      checkNumber();
+    } else if (userNumber < botNumber) {
+      attempts--;
+      alert("Загаданное число больше, осталось попыток " + attempts);
+      checkNumber();
+    } else {
+      if (confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?")) {
+        guessNumber();
+      } else {
+        alert("Спасибо за игру!");
+      }
+    }
+  };
+  checkNumber();
 };
 
-console.log(
-  checkString(
-    " Скажи-ка, дядя, ведь не даром Москва, спаленная пожаром, Французу отдана?  "
-  )
-);
+guessNumber();
